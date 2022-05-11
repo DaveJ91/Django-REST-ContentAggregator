f70d83655f9c2d474037b3d4829e342fbdc0547c
DaveJ91 <david.omeara91@gmail.com>
2022-05-11 23:33:56 +0100

    put main scaffolding in place

--- contentApp/models.py
+++ contentApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Content(models.Model):
+    title = models.CharField(max_length=100)
+    source = models.CharField(max_length=30)
+    topic = models.CharField(max_length=30)
+    date = models.DateField()
---
+++ contentApp/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from .models import Content
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+class ContentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Content
+        fields = ['title', 'source', 'topic', 'date']
